e751ab8acd15b87a6b365ec2adebfb530384dcbc
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-21 11:08:58 +1000

    admin chapters management tests

--- cypress/e2e/admin-books-table.cy.ts
+++ cypress/e2e/admin-books-table.cy.ts
@@ -1,14 +1,14 @@
-before(() => {
-  cy.clearDB();
-  cy.prepareAdmin();
-  cy.seedBooks(10);
-});
-
 beforeEach(() => {
   cy.loadAdminSession();
 });
 
 describe("Admin Books Page", () => {
+  before(() => {
+    cy.clearDB();
+    cy.prepareAdmin();
+    cy.seedBooks(10);
+  });
+
   it("should render books table", () => {
     cy.visit("localhost:3000/admin/books");
 
@@ -47,7 +47,6 @@ describe("Admin Books Page", () => {
 
   it("should be able to delete a book", () => {
     cy.visit("localhost:3000/admin/books");
-    cy.wait(2000);
     cy.get("input[placeholder='Search by book name']")
       .clear()
       .type("E2E-Modified");
@@ -59,5 +58,149 @@ describe("Admin Books Page", () => {
         cy.contains("Remove").click();
       });
     cy.get('[data-testid="book-entry"]').should("have.length", 0);
+  });
+});
+
+describe("Edit Chapters tests", () => {
+  before(() => {
+    cy.clearDB();
+    cy.prepareAdmin();
+  });
+
+  it("should render books table", () => {
+    cy.visit("localhost:3000/admin/books");
+
+    cy.get('[data-testid="book-entry"]').should("have.length", 0);
+  });
+
+  it("should be able to add a book", () => {
+    cy.visit("localhost:3000/admin/books");
+    cy.contains("Add book").click();
+    cy.contains("Add book").click();
+    cy.get(".modal-box").should("be.visible");
+    cy.get("input[name='name']").clear().type("E2E-Modified");
+    cy.contains("Save").click();
+    cy.contains("E2E-Modified");
+  });
+
+  it("should be able to add a chapter", () => {
+    cy.visit("localhost:3000/admin/books");
+    cy.contains("Modify").click();
+    cy.contains("Modify").click();
+    cy.get(".modal-box").should("be.visible");
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1");
+    cy.contains("Add Chapter").click();
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 3");
+    cy.contains("Add Chapter").click();
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 2");
+    cy.contains("Add Chapter").click();
+    cy.contains("Save").click();
+
+    cy.contains("Chapter 1");
+    cy.contains("Chapter 2");
+    cy.contains("Chapter 3");
+  });
+
+  it("should be able to edit order of chapter", () => {
+    cy.visit("localhost:3000/admin/books");
+    cy.contains("Modify").click();
+    cy.contains("Modify").click();
+    cy.get(".modal-box").should("be.visible");
+
+    cy.get('[data-testid="chapter-entry"]').then(($elements) => {
+      const orders = $elements
+        .map((_, el) => {
+          return el.innerText;
+        })
+        .get()
+        .join(",");
+      expect(orders).to.equal("Chapter 1,Chapter 3,Chapter 2");
+    });
+
+    cy.get('[data-testid="chapter-entry"]')
+      .eq(1)
+      .within(() => {
+        cy.get('[data-testid="move-chapter-down"]').click();
+      });
+
+    cy.contains("Save").click();
+    cy.contains("Modify").click();
+    cy.contains("Modify").click();
+    cy.get('[data-testid="chapter-entry"]').then(($elements) => {
+      const orders = $elements
+        .map((_, el) => {
+          return el.innerText;
+        })
+        .get()
+        .join(",");
+      expect(orders).to.equal("Chapter 1,Chapter 2,Chapter 3");
+    });
+  });
+
+  it("should be able to delete chapters", () => {
+    cy.visit("localhost:3000/admin/books");
+    cy.contains("Modify").click();
+    cy.contains("Modify").click();
+    cy.get(".modal-box").should("be.visible");
+
+    cy.get('[data-testid="remove-chapter"]').first().click();
+    cy.get('[data-testid="remove-chapter"]').first().click();
+    cy.get('[data-testid="remove-chapter"]').first().click();
+
+    cy.contains("Save").click();
+
+    cy.contains("Modify").click();
+    cy.contains("Modify").click();
+    cy.get('[data-testid="chapter-entry"]').should("have.length", 0);
+  });
+
+  it("should be able to add nested chapters", () => {
+    cy.visit("localhost:3000/admin/books");
+    cy.contains("Modify").click();
+    cy.contains("Modify").click();
+    cy.get(".modal-box").should("be.visible");
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1");
+    cy.contains("Add Chapter").click();
+
+    cy.get('[data-testid="chapter-entry"]')
+      .first()
+      .within(() => {
+        cy.get('[data-testid="chapter-dropdown"]').click();
+      });
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1.1");
+    cy.contains("Add Chapter").click();
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1.2");
+    cy.contains("Add Chapter").click();
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1.3");
+    cy.contains("Add Chapter").click();
+
+    cy.get('[data-testid="chapter-entry"]')
+      .contains("Chapter 1.2")
+      .within(() => {
+        cy.get('[data-testid="chapter-dropdown"]').click();
+      });
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1.2.1");
+    cy.contains("Add Chapter").click();
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1.2.2");
+    cy.contains("Add Chapter").click();
+
+    cy.contains("Add new chapter").click();
+    cy.get("input[placeholder='Chapter ...']").clear().type("Chapter 1.2.3");
+    cy.contains("Add Chapter").click();
   });
 });
--- src/components/admin/table/books-table/components/chapter-badge/index.tsx
+++ src/components/admin/table/books-table/components/chapter-badge/index.tsx
@@ -149,12 +149,13 @@ export const ChapterBadge: React.FC<Props> = ({
   );
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center" data-testid="chapter-entry">
       <DropdownIcon
         width={20}
         height={20}
         className="cursor-pointer"
         onDropdownToggle={onDropdownToggle}
+        data-testid="chapter-dropdown"
       />
       <div
         {...props}
@@ -162,6 +163,7 @@ export const ChapterBadge: React.FC<Props> = ({
       >
         {chapter.name}
         <svg
+          data-testid="remove-chapter"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -177,6 +179,7 @@ export const ChapterBadge: React.FC<Props> = ({
         </svg>
         {currentChapterOrder > minOrder && (
           <svg
+            data-testid="move-chapter-up"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 50 50"
             className="w-4 h-4 inline-block stroke-current cursor-pointer"
@@ -187,6 +190,7 @@ export const ChapterBadge: React.FC<Props> = ({
         )}
         {currentChapterOrder < maxOrder && (
           <svg
+            data-testid="move-chapter-down"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 50 50"
             className="w-4 h-4 inline-block stroke-current cursor-pointer"
