f3e6cbae208b6d0b106238bfdf5488932654212c
ducduyn31 <ducduy.ababat@gmail.com>
2023-08-25 22:14:43 +1000

    setup github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,6 +9,14 @@ export default defineConfig({
     supportFile: "cypress/support/component.tsx",
   },
 
+  env: {
+    MAILTRAP_TOKEN: "",
+    MAILTRAP_ACCOUNT_ID: "",
+    MAILTRAP_INBOX_ID: "",
+    SUPABASE_URL: "",
+    SUPABASE_KEY: "",
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
