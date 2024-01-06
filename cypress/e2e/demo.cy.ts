6bcd135c87c8daaf2bd29733b96515c73675bb60
ducduyn31 <ducduy.ababat@gmail.com>
2024-01-06 21:49:47 +1100

    temporary fix the loading issue (#9)
    * temporary fix the loading issue
    
    * temporary fix the loading issue
    
    * temporary fix the loading issue
    
    * temporary fix the loading issue
    
    * temporary fix the loading issue
    
    * temporary fix the loading issue
    
    * temporary fix the loading issue

--- cypress/e2e/demo.cy.ts
+++ cypress/e2e/demo.cy.ts
@@ -1,5 +1,21 @@
 describe("Seed Users", () => {
-  it("should seed users", () => {
+  it.skip("should seed users", () => {
     cy.seedUsers(1000);
+  });
+
+  it.skip("should seed admin", () => {
+    cy.seedAdmin();
+  });
+
+  it.skip("should seed books", () => {
+    cy.seedBooks(10);
+  });
+
+  it("should seed questions", () => {
+    cy.seedQuestions(1000);
+  });
+
+  it.skip("should clear db", () => {
+    cy.clearDB();
   });
 });
